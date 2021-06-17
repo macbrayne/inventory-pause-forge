276ede7f6f8e5fa99a8e7db6620f5c28cfbccffc
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2021-06-17 10:08:49 +0200

    add class name debug screen

--- src/main/java/de/macbrayne/forge/inventorypause/InventoryPause.java
+++ src/main/java/de/macbrayne/forge/inventorypause/InventoryPause.java
@@ -1,10 +1,17 @@
 package de.macbrayne.forge.inventorypause;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import de.macbrayne.forge.inventorypause.utils.ForgeConfigHelper;
 import de.macbrayne.forge.inventorypause.utils.ModConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -23,5 +30,20 @@ public class InventoryPause {
 
         ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ForgeConfigHelper::registerConfig);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void onGUIDrawPost(GuiScreenEvent.DrawScreenEvent.Post event){
+        if(MOD_CONFIG.debug) {
+            Screen screen = event.getGui();
+
+            int line = 0;
+            for(Class cl = screen.getClass(); cl.getSuperclass() != null; cl = cl.getSuperclass()) {
+                Minecraft.getInstance().fontRenderer.drawStringWithShadow(new MatrixStack(), cl.getName(), 1f, 1f + 10 * line, 0xffffffff);
+                line++;
+            }
+        }
     }
 }
