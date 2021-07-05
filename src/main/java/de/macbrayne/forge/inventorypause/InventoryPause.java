ab570e17b006af3878283394d7a9ff8a1937d610
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2021-07-05 21:32:47 +0200

    move Gui events from InventoryPause into ForgeGuiEvents

--- src/main/java/de/macbrayne/forge/inventorypause/InventoryPause.java
+++ src/main/java/de/macbrayne/forge/inventorypause/InventoryPause.java
@@ -1,19 +1,12 @@
 package de.macbrayne.forge.inventorypause;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
 import de.macbrayne.forge.inventorypause.common.ModConfig;
-import de.macbrayne.forge.inventorypause.common.ScreenHelper;
 import de.macbrayne.forge.inventorypause.utils.ForgeConfigHelper;
 import de.macbrayne.forge.inventorypause.utils.ModCompatHelper;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.EventPriority;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -22,13 +15,10 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 import org.apache.commons.lang3.tuple.Pair;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("inventorypause")
 public class InventoryPause {
-    private static final Logger LOGGER = LogManager.getLogger("inventorypause");
     public static ModConfig MOD_CONFIG = new ModConfig();
 
     public InventoryPause() {
@@ -40,26 +30,6 @@ public class InventoryPause {
         DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ForgeConfigHelper::registerConfig);
 
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void onOpenGUI(GuiScreenEvent.DrawScreenEvent.InitGuiEvent.Pre event) {
-        if(MOD_CONFIG.debug && !ScreenHelper.isConfiguredScreen(event.getGui())) {
-            LOGGER.info(event.getGui().getClass().getName());
-        }
-    }
-
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void onGUIDrawPost(GuiScreenEvent.DrawScreenEvent.Post event){
-        if(MOD_CONFIG.debug) {
-            Screen screen = event.getGui();
-
-            int line = 0;
-            for(Class<?> cl = screen.getClass(); cl.getSuperclass() != null && line < MOD_CONFIG.debugText.maxDepth; cl = cl.getSuperclass()) {
-                Minecraft.getInstance().fontRenderer.drawStringWithShadow(new MatrixStack(), cl.getName(), MOD_CONFIG.debugText.x, MOD_CONFIG.debugText.y + 10 * line, 0xffffffff);
-                line++;
-            }
-        }
     }
 
     private void enqueueIMC(InterModEnqueueEvent event) {
---
+++ src/main/java/de/macbrayne/forge/inventorypause/utils/ForgeGuiEvents.java
@@ -0,0 +1,40 @@
+package de.macbrayne.forge.inventorypause.utils;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+import de.macbrayne.forge.inventorypause.common.ScreenHelper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static de.macbrayne.forge.inventorypause.InventoryPause.MOD_CONFIG;
+
+@Mod.EventBusSubscriber(modid = "inventorypause", bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+public class ForgeGuiEvents {
+    private static final Logger LOGGER = LogManager.getLogger("inventorypause");
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void onOpenGUI(GuiScreenEvent.DrawScreenEvent.InitGuiEvent.Pre event) {
+        if(MOD_CONFIG.debug && !ScreenHelper.isConfiguredScreen(event.getGui())) {
+            LOGGER.info(event.getGui().getClass().getName());
+        }
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public static void onGUIDrawPost(GuiScreenEvent.DrawScreenEvent.Post event){
+        if(MOD_CONFIG.debug) {
+            Screen screen = event.getGui();
+
+            int line = 0;
+            for(Class<?> cl = screen.getClass(); cl.getSuperclass() != null && line < MOD_CONFIG.debugText.maxDepth; cl = cl.getSuperclass()) {
+                Minecraft.getInstance().fontRenderer.drawStringWithShadow(new MatrixStack(), cl.getName(), MOD_CONFIG.debugText.x, MOD_CONFIG.debugText.y + 10 * line, 0xffffffff);
+                line++;
+            }
+        }
+    }
+}
