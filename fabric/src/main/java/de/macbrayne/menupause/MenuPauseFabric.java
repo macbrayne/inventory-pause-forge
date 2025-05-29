cbd3ef3a7cab8bd9d21dfb2adacd99c4d4c02da1
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2025-05-29 20:57:44 +0200

    Fix datagen failing on Fabric due to the main entrypoint getting executed and loading the non-existant config

--- fabric/src/main/java/de/macbrayne/menupause/MenuPauseFabric.java
+++ fabric/src/main/java/de/macbrayne/menupause/MenuPauseFabric.java
@@ -10,6 +10,10 @@ import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 public class MenuPauseFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        // To prevent it from crashing during data generation as we try to load data that hasn't yet been generated.
+        if (System.getProperty("fabric-api.datagen") != null) {
+            return;
+        }
         FabricEvents.registerKeybindings();
         ClientTickEvents.END_CLIENT_TICK.register(FabricEvents::endClientTick);
         ScreenEvents.BEFORE_INIT.register(FabricEvents::screenInit);
